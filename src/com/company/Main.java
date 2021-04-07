ecc659c25aa68a10e80de39ca6bcf0a6cd5530a9
AbylovErmek <gskro705@gmail.com>
2021-04-07 22:55:09 +0600

    New

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,13 +6,13 @@ public class Main {
         BankAccount user = new BankAccount();
         user.deposit(20000.0);
         System.out.println("На счету: " + user.getAmount());
-        while (true){
+        while (true) {
             try {
-                System.out.println(("Списано со счёта: " + "-") + user.withdraw(6000));
-            }catch (LimitException e){
+                System.out.println(("Осталось на счету: ") + user.withdraw(6000));
+            } catch (LimitException e) {
                 System.out.println("Осталось на счету: " + e.getRemainingAmount());
                 System.out.println(e.getMessage());
-                System.out.println("Осталось на счету: " + (user.getAmount() - e.getRemainingAmount()));
+                System.out.println("Осталось на счету: " + (user.withdraw((int) e.getRemainingAmount())));
                 break;
             }
         }
